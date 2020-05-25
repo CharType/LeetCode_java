b1a6bd4ce04d88cac585f108e7bb0de956df462a
chengqain <chengqian@szy.cn>
2020-05-26 00:15:26 +0800

    完成2数之和

--- src/com/高频题/_1_两数之和.java
+++ src/com/高频题/_1_两数之和.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class _1_两数之和 {
     public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length <= 2) return null;
+        if (nums == null || nums.length < 2) return null;
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0;i<nums.length;i++) {
             Integer index = map.get(target - nums[i]);
@@ -21,7 +21,7 @@ public class _1_两数之和 {
 
     public static void main(String[] args) {
         _1_两数之和 o = new _1_两数之和();
-        int [] nums = o.twoSum(new int[]{2, 7, 11, 15},18);
+        int [] nums = o.twoSum(new int[]{3, 3},6);
         Integers.println(nums);
     }
 }
