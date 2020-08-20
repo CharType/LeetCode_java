0d79cd4eb069d339268c8fce1eadc0cad087f8b3
chengqain <chengqian@szy.cn>
2020-08-20 16:36:59 +0800

    添加颜色计算方案

---
+++ src/com/Colors/Main.java
@@ -0,0 +1,20 @@
+package com.Colors;
+
+public class Main {
+    public static void main(String[] args) {
+        rgba();
+    }
+
+    public static void rgba() {
+        System.out.println("透明度 | 十六进制");
+        System.out.println("---- | ----");
+        for (double i = 1; i >= 0; i -= 0.01) {
+            i = Math.round(i * 100) / 100.0d;
+            int alpha = (int) Math.round(i * 255);
+            String hex = Integer.toHexString(alpha).toUpperCase();
+            if (hex.length() == 1) hex = "0" + hex;
+            int percent = (int) (i * 100);
+            System.out.println(String.format("%d%% | %s", percent, hex));
+        }
+    }
+}
