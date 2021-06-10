642b77be94ea6886448aec509e30feca9b580111
chengqian <chartype@163.com>
2021-06-10 23:46:32 +0800

    添加单向循环链表的测试结果

--- src/com/cq/source/Main.java
+++ src/com/cq/source/Main.java
@@ -3,7 +3,7 @@ package com.cq.source;
 //import java.util.LinkedList;
 //import java.util.List;
 
-import com.cq.source.single.SingleLinkList;
+import com.cq.source.circle.SingleCircleLinkList;
 import com.tools.Asserts;
 
 public class Main {
@@ -13,14 +13,20 @@ public class Main {
         list.add(22);
         list.add(33);
         list.add(44);
+        
+//        System.out.println(list);
 
         list.add(0, 55); // [55, 11, 22, 33, 44]
+
+
         list.add(2, 66); // [55, 11, 66, 22, 33, 44]
         list.add(list.size(), 77); // [55, 11, 66, 22, 33, 44, 77]
+
 
         list.remove(0); // [11, 66, 22, 33, 44, 77]
         list.remove(2); // [11, 66, 33, 44, 77]
         list.remove(list.size() - 1); // [11, 66, 33, 44]
+//        System.out.println(list);
 
         Asserts.test(list.indexof(44) == 3);
         Asserts.test(list.indexof(22) == List.ELEMENT_NOT_FOUND);
@@ -36,19 +42,9 @@ public class Main {
     public static void main(String[] args) {
 
         //testList(new LinkedList<>());
-        testList(new SingleLinkList<>());
-//        List<Integer> list = new LinkedList<>();
-//        for (int i = 0; i < 10; i++) {
-//            list.add(i);
-//        }
-//        System.out.println(list.toString());
-//        list.set(5, 10);
-//        Asserts.test(list.get(5) == 10);
-//        list.add(0, 100);
-//        Asserts.test(list.get(0) == 100);
-//        Asserts.test(list.size() == 11);
-//        list.remove(8);
-//        Asserts.test(list.size() == 10);
+//        testList(new SingleLinkList<>());
+        testList(new SingleCircleLinkList<>());
+        System.out.println(123);
 
 
     }
