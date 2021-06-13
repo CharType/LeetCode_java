dc9f8bc6908b6b055903ce6e14aa1f382d95b1ea
chengqian <chartype@163.com>
2021-06-13 23:09:48 +0800

    有效的括号提交

--- src/com/cq/source/stack/Stack.java
+++ src/com/cq/source/stack/Stack.java
@@ -1,4 +1,49 @@
 package com.cq.source.stack;
 
-public class Stack {
+import com.cq.source.list.List;
+import com.cq.source.list.array.ArrayList;
+
+public class Stack<E> {
+    private List<E> list = new ArrayList<>();
+
+    // 栈中元素的数量
+    public int size() {
+        return this.list.size();
+    }
+
+    // 是否为空
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+    // 入栈
+    public void push(E element) {
+        this.list.add(element);
+    }
+
+    // 出栈
+    public E pop() {
+        return this.list.remove(list.size() - 1);
+    }
+
+    // 查看栈顶元素
+    public E peek() {
+        return this.list.get(list.size() - 1);
+    }
+
+    // 清空栈
+    public void clear() {
+        this.list.clear();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        while (!this.isEmpty()) {
+            sb.append(" " + this.pop());
+        }
+        sb.append(']');
+        return sb.toString();
+    }
 }
---
+++ src/com/cq/source/stack/leetCode/_856_括号的分数.java
@@ -0,0 +1,8 @@
+package com.cq.source.stack.leetCode;
+
+public class _856_括号的分数 {
+    
+    public static void main(String[] args) {
+
+    }
+}
