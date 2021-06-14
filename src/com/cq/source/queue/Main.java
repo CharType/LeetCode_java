c3f4927b681e17ed35c555c79f21320586e40146
chengqian <chartype@163.com>
2021-06-15 07:11:58 +0800

    添加双端队列

--- src/com/cq/source/queue/Main.java
+++ src/com/cq/source/queue/Main.java
@@ -2,6 +2,22 @@ package com.cq.source.queue;
 
 public class Main {
     public static void main(String[] args) {
+        testDeQue();
+    }
+
+
+    public static void testDeQue() {
+        Deque<Integer> deque = new Deque<>();
+        for (int i = 0; i < 5; i++) {
+            deque.enQueueRear(i + 10);
+            deque.enQueueFront(i + 11);
+        }
+        while (!deque.isEmpty()) {
+            System.out.println(deque.deQueueFront());
+        }
+    }
+
+    public static void testQueue() {
         Queue<Integer> queue = new Queue<>();
         for (int i = 0; i < 10; i++) {
             queue.enQueue(i);
