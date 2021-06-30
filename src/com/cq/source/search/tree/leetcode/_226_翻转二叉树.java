1ad29c3b408766fce5f53be1a1a3e500a9240227
chengqian <chartype@163.com>
2021-06-30 21:36:38 +0800

    翻转二叉树 迭代方式

--- src/com/cq/source/search/tree/leetcode/_226_翻转二叉树.java
+++ src/com/cq/source/search/tree/leetcode/_226_翻转二叉树.java
@@ -3,6 +3,9 @@ package com.cq.source.search.tree.leetcode;
 import com.TreeNode.TreeNode;
 import com.TreeNode.printer.BinaryTrees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode-cn.com/problems/invert-binary-tree/
  */
@@ -17,6 +20,28 @@ public class _226_翻转二叉树 {
         return root;
     }
 
+    public TreeNode invertTree1(TreeNode root) {
+        if (root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
@@ -25,10 +50,10 @@ public class _226_翻转二叉树 {
         root.left.right = new TreeNode(3);
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(9);
-        com.other._226_翻转二叉树 o = new com.other._226_翻转二叉树();
+        _226_翻转二叉树 o = new _226_翻转二叉树();
         System.out.println("翻转前：");
         BinaryTrees.print(root);
-        TreeNode newTree = o.invertTree(root);
+        TreeNode newTree = o.invertTree1(root);
         System.out.println('\n');
         System.out.println("翻转后：");
         BinaryTrees.print(newTree);
