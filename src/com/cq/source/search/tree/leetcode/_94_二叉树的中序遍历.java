26cb858a7c7e38a46fb8e5ca0dbccbe2b9b0efb3
chengqian <chartype@163.com>
2021-07-02 10:32:52 +0800

    中序遍历递归方式

--- src/com/cq/source/search/tree/leetcode/_94_二叉树的中序遍历.java
+++ src/com/cq/source/search/tree/leetcode/_94_二叉树的中序遍历.java
@@ -1,7 +1,42 @@
 package com.cq.source.search.tree.leetcode;
 
+import com.TreeNode.TreeNode;
+import com.TreeNode.printer.BinaryTrees;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
  */
 public class _94_二叉树的中序遍历 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list;
+    }
+
+    public void inorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        inorderTraversal(node.left, list);
+        list.add(node.val);
+        inorderTraversal(node.right, list);
+    }
+
+    public static void main(String[] args) {
+        _94_二叉树的中序遍历 o = new _94_二叉树的中序遍历();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        BinaryTrees.print(root);
+        System.out.println('\n');
+        System.out.println(o.inorderTraversal(root));
+        System.out.println('\n');
+//        System.out.println(o.postorderTraversalStack(root));
+    }
 }
