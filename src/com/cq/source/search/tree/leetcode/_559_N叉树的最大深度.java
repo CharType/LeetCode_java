f2b29fd76ba2ddc92af4598866cb2b5fc60c1849
chengqian <chartype@163.com>
2021-07-02 22:17:08 +0800

    N叉树 求深度 递归方式

--- src/com/cq/source/search/tree/leetcode/_559_N叉树的最大深度.java
+++ src/com/cq/source/search/tree/leetcode/_559_N叉树的最大深度.java
@@ -4,4 +4,22 @@ package com.cq.source.search.tree.leetcode;
  * https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
  */
 public class _559_N叉树的最大深度 {
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+
+        return maxDepth(root, 1);
+    }
+
+    public int maxDepth(Node node, int parentDepth) {
+        if (node == null) return parentDepth;
+        int depth = 0;
+        for (int i = 0; i < node.children.size(); i++) {
+            depth = Math.max(maxDepth(node.children.get(i), parentDepth + 1), depth);
+        }
+        return depth + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
