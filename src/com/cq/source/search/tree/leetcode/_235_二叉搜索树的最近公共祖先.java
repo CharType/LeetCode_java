6012f0d4c147b8cee8155ba2d73d6915cf546cd5
chengqian <chartype@163.com>
2021-07-04 23:01:47 +0800

    _235_二叉搜索树的最近公共祖先 未完成提交
    部分case测试不通过

--- src/com/cq/source/search/tree/leetcode/_235_二叉搜索树的最近公共祖先.java
+++ src/com/cq/source/search/tree/leetcode/_235_二叉搜索树的最近公共祖先.java
@@ -1,7 +1,24 @@
 package com.cq.source.search.tree.leetcode;
 
+import com.TreeNode.TreeNode;
+
 /**
  * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/
  */
 public class _235_二叉搜索树的最近公共祖先 {
+    //todo:未完成
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+        if (leftNode != null && rightNode != null) return root;
+        if (leftNode != null && rightNode == null) return leftNode;
+        if (leftNode == null && rightNode != null) return rightNode;
+        if (root == p || root == q) return root;
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
