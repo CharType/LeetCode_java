292304c00c6fc3ed13a86b6143588782f20015c9
chengqian <chartype@163.com>
2021-07-05 22:24:55 +0800

    添加二叉堆文件

---
+++ src/com/cq/source/heap/BinaryHeap.java
@@ -0,0 +1,38 @@
+package com.cq.source.heap;
+
+public class BinaryHeap<E> implements Heap {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public Object get() {
+        return null;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object replace(Object element) {
+        return null;
+    }
+}
