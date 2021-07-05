9efb6358e7ab20e435293a488fb624b1f68b45a4
chengqian <chartype@163.com>
2021-07-05 13:09:19 +0800

    完善

--- src/com/cq/source/search/tree/leetcode/_235_二叉搜索树的最近公共祖先.java
+++ src/com/cq/source/search/tree/leetcode/_235_二叉搜索树的最近公共祖先.java
@@ -6,16 +6,12 @@ import com.TreeNode.TreeNode;
  * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/
  */
 public class _235_二叉搜索树的最近公共祖先 {
-    //todo:未完成
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || p == root || q == root) return root;
         TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
         TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
         if (leftNode != null && rightNode != null) return root;
-        if (leftNode != null && rightNode == null) return leftNode;
-        if (leftNode == null && rightNode != null) return rightNode;
-        if (root == p || root == q) return root;
-        return null;
+        return (leftNode != null) ? leftNode : rightNode;
     }
 
     public static void main(String[] args) {
