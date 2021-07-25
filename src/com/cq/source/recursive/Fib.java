8bfb9c64d06c808f6f94e69a0b86df2a310c393b
chengqian <chartype@163.com>
2021-07-26 01:00:58 +0800

    递归开发完成

--- src/com/cq/source/recursive/Fib.java
+++ src/com/cq/source/recursive/Fib.java
@@ -6,13 +6,13 @@ import com.tools.Times.Task;
 public class Fib {
     public static void main(String[] args) {
         Fib f = new Fib();
-        int n = 45;
-        Times.test("fib", new Task() {
-            @Override
-            public void execute() {
-                System.out.println(f.fib(n));
-            }
-        });
+        int n = 64;
+//        Times.test("fib", new Task() {
+//            @Override
+//            public void execute() {
+//                System.out.println(f.fib(n));
+//            }
+//        });
 
         Times.test("fib1", new Task() {
             @Override
@@ -32,6 +32,25 @@ public class Fib {
             @Override
             public void execute() {
                 System.out.println(f.fib3(n));
+            }
+        });
+
+        Times.test("fib4", new Task() {
+            @Override
+            public void execute() {
+                System.out.println(f.fib4(n));
+            }
+        });
+        Times.test("fib5", new Task() {
+            @Override
+            public void execute() {
+                System.out.println(f.fib5(n));
+            }
+        });
+        Times.test("fib6", new Task() {
+            @Override
+            public void execute() {
+                System.out.println(f.flb6(n));
             }
         });
     }
@@ -63,6 +82,16 @@ public class Fib {
 
     public int fib3(int n) {
         if (n <= 2) return 1;
+        int[] array = new int[2];
+        array[0] = array[1] = 1;
+        for (int i = 3; i <= n; i++) {
+            array[i & 1] = array[0] + array[1];
+        }
+        return array[n & 1];
+    }
+
+    public int fib4(int n) {
+        if (n <= 2) return 1;
         int first = 1;
         int second = 1;
         for (int i = 3; i <= n; i++) {
@@ -70,5 +99,20 @@ public class Fib {
             first = second - first;
         }
         return second;
+    }
+
+    public int fib5(int n) {
+        double c = Math.sqrt(5);
+
+        return (int) ((Math.pow((1 + c) / 2, n) - Math.pow((1 - c) / 2, n)) / c);
+    }
+
+    public int flb6(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        double sqrt5 = Math.sqrt(5);
+        double result = 1 / sqrt5 * (Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n));
+        return (int) Math.round(result);
+
     }
 }
