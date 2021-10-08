a0f968ef70be2517f12ae041e533dbb03c3ef4da
chengqian <chartype@163.com>
2021-10-08 20:18:13 +0800

    最长上升子序列完善

--- src/com/cq/source/DynamicProgramming/_300_最长递增子序列.java
+++ src/com/cq/source/DynamicProgramming/_300_最长递增子序列.java
@@ -1,6 +1,7 @@
 package com.cq.source.DynamicProgramming;
 
 /**
+ * 严格上升
  * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
  * <p>
  * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
@@ -16,17 +17,18 @@ public class _300_最长递增子序列 {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return 1;
         int[] dp = new int[nums.length];
-        dp[0] = 1;
-        int maxLength = 0;
+        int maxLength = dp[0] = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                dp[i] = dp[i - 1] + 1;
-            } else {
-                dp[i] = 1;
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
+
             maxLength = Math.max(maxLength, dp[i]);
         }
-        return dp[nums.length];
+        return maxLength;
     }
 
     public static void main(String[] args) {
