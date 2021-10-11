a4b027b7b37c15416d93d15ca2e38e3af1eaddc0
chengqian <chartype@163.com>
2021-10-11 18:09:15 +0800

    礼物的最大的价值

--- src/com/cq/source/DynamicProgramming/背包_01.java
+++ src/com/cq/source/DynamicProgramming/背包_01.java
@@ -10,7 +10,44 @@ public class 背包_01 {
         int[] values = {6, 3, 5, 4, 6};
         int[] weights = {2, 2, 6, 5, 4};
         int capacity = 10;
-        System.out.println(maxValue1(values, weights, capacity));
+        System.out.println(maxValue3(values, weights, capacity));
+        System.out.println(maxValue2(values, weights, capacity));
+    }
+
+    /**
+     * @param values
+     * @param widgets
+     * @param capacity
+     * @return 返回-1 表示没有办法正好凑够capacity这个容量
+     */
+    static int maxValue3(int[] values, int[] widgets, int capacity) {
+        if (values == null || values.length == 0) return 0;
+        if (widgets == null || widgets.length == 0) return 0;
+        if (values.length != widgets.length || capacity <= 0) return 0;
+        int[] dp = new int[capacity + 1];
+        for (int j = 1; j <= capacity; j++) {
+            dp[j] = Integer.MIN_VALUE;
+        }
+
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = capacity; j >= widgets[i - 1]; j--) {
+                dp[j] = Math.max(dp[j], values[i - 1] + dp[j - widgets[i - 1]]);
+            }
+        }
+        return dp[capacity] < 0 ? -1 : dp[capacity];
+    }
+
+    static int maxValue2(int[] values, int[] widgets, int capacity) {
+        if (values == null || values.length == 0) return 0;
+        if (widgets == null || widgets.length == 0) return 0;
+        if (values.length != widgets.length || capacity <= 0) return 0;
+        int[] dp = new int[capacity + 1];
+        for (int i = 1; i <= values.length; i++) {
+            for (int j = capacity; j >= widgets[i - 1]; j--) {
+                dp[j] = Math.max(dp[j], values[i - 1] + dp[j - widgets[i - 1]]);
+            }
+        }
+        return dp[capacity];
     }
 
     static int maxValue1(int[] values, int[] widgets, int capacity) {
