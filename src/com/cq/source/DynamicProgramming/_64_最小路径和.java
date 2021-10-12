63ec9f5a809e9dca045e5a9f0cd66fe244a06866
chengqian <chartype@163.com>
2021-10-12 11:03:39 +0800

    提交

--- src/com/cq/source/DynamicProgramming/_64_最小路径和.java
+++ src/com/cq/source/DynamicProgramming/_64_最小路径和.java
@@ -1,2 +1,27 @@
-package com.cq.source.DynamicProgramming;public class _64_最小路径和 {
+package com.cq.source.DynamicProgramming;
+
+public class _64_最小路径和 {
+    static int minPathSum(int[][] grid) {
+        if (grid == null || grid[0] == null || grid[0].length == 0) return 0;
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            int[] nums = grid[i];
+            for (int j = 1; j < nums.length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(minPathSum(grid));
+    }
 }
