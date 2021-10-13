25e29c2e3bcf5f8dc59f7f3f007b37d198882bd0
chengqian <chartype@163.com>
2021-10-13 20:51:31 +0800

    最长回文串算法提交

--- src/com/cq/source/DynamicProgramming/_5_最长回文子串.java
+++ src/com/cq/source/DynamicProgramming/_5_最长回文子串.java
@@ -25,11 +25,77 @@ package com.cq.source.DynamicProgramming;
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * dp[i][j] = 字符串以i开头 以j结尾的字符串是否是最长回文字符串
  */
 public class _5_最长回文子串 {
 
-    public static void main(String[] args) {
 
+    static String longestPalindrome(String s) {
+        if (s == null) return null;
+        char[] chars = s.toCharArray();
+        if (chars.length == 0) return "";
+        boolean[][] dp = new boolean[chars.length][chars.length];
+        int maxLength = 0;
+        int begin = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            for (int j = i; j < chars.length; j++) {
+                int length = j - i + 1;
+                dp[i][j] = chars[i] == chars[j] && (length <= 2 || dp[i + 1][j - 1]);
+                if (dp[i][j] && length > maxLength) {
+                    maxLength = length;
+                    begin = i;
+                }
+            }
+        }
+        return new String(chars, begin, maxLength);
+    }
+
+    /**
+     * 扩展中心法
+     *
+     * @param s
+     * @return
+     */
+    static String longestPalindrome2(String s) {
+        if (s == null) return s;
+        char[] chars = s.toCharArray();
+        if (chars.length <= 1) return s;
+        int maxLength = 1;
+        int begin = 0;
+        for (int i = chars.length - 2; i >= 1; i--) {
+            int len1 = palindromeLength(chars, i - 1, i + 1);
+            int len2 = palindromeLength(chars, i, i + 1);
+            len1 = Math.max(len1, len2);
+            if (len1 > maxLength) {
+                maxLength = len1;
+                begin = i - ((maxLength - 1) >> 1);
+            }
+        }
+
+        if (chars[0] == chars[1] && maxLength < 2) {
+            maxLength = 2;
+            begin = 0;
+        }
+
+        return new String(chars, begin, maxLength);
+    }
+
+    /**
+     * @param cs
+     * @param l
+     * @param r
+     * @return
+     */
+    private static int palindromeLength(char[] cs, int l, int r) {
+        while (l >= 0 && r < cs.length && cs[l] == cs[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("abcdefedcba"));
+        System.out.println(longestPalindrome2("abcdefedcba"));
     }
 }
