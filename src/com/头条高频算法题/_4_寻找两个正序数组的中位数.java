b40ac736852724ec107709f25d98e67fd23b91b3
chengqian <chartype@163.com>
2022-01-11 17:48:03 +0800

    提交

--- src/com/头条高频算法题/_4_寻找两个正序数组的中位数.java
+++ src/com/头条高频算法题/_4_寻找两个正序数组的中位数.java
@@ -4,14 +4,14 @@ package com.头条高频算法题;
  * 4. 寻找两个正序数组的中位数
  * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
  * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
- *
+ * <p>
  * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- *
+ * <p>
  * 你可以假设 nums1 和 nums2 不会同时为空。
  */
 public class _4_寻找两个正序数组的中位数 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int [] meargeList = new int[nums1.length + nums2.length];
+        int[] meargeList = new int[nums1.length + nums2.length];
         int index1 = 0;
         int index2 = 0;
         int i = 0;
@@ -21,7 +21,7 @@ public class _4_寻找两个正序数组的中位数 {
             if (nums1[index1] < nums2[index2]) {
                 meargeList[i] = nums1[index1];
                 index1++;
-            } else  {
+            } else {
                 meargeList[i] = nums2[index2];
                 index2++;
             }
@@ -41,7 +41,7 @@ public class _4_寻找两个正序数组的中位数 {
 
         if (isq == 1) {
             return meargeList[meargeList.length >> 1];
-        } else  {
+        } else {
             return (meargeList[(meargeList.length >> 1) - 1] + meargeList[meargeList.length >> 1]) / 2.0;
         }
 
@@ -50,9 +50,9 @@ public class _4_寻找两个正序数组的中位数 {
 
     public static void main(String[] args) {
         _4_寻找两个正序数组的中位数 o = new _4_寻找两个正序数组的中位数();
-        int [] nums1 = {1, 3};
-        int [] nums2 = {2,5};
-        System.out.println(o.findMedianSortedArrays(nums1,nums2));
-        System.out.println(Integer.toString(-121));
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2, 5};
+        System.out.println(o.findMedianSortedArrays(nums1, nums2));
+//        System.out.println(Integer.toString(-121));
     }
 }
