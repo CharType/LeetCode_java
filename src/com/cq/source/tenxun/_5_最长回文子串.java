cab5818790b84fd623c7110afce75d5d5df5af85
chengqian <chartype@163.com>
2022-01-11 18:19:35 +0800

    最长回文子串 扩展中心法

--- src/com/cq/source/tenxun/_5_最长回文子串.java
+++ src/com/cq/source/tenxun/_5_最长回文子串.java
@@ -25,6 +25,32 @@ public class _5_最长回文子串 {
         return new String(chars, li, max);
     }
 
+    // 扩展中心法
+    public String longestPalindrome2(String s) {
+        if (s == null) return s;
+        char[] chars = s.toCharArray();
+        if (chars.length <= 1) return s;
+        int maxLength = 1;
+        int begin = 0;
+        int i = 0;
+        while (i < chars.length) {
+            int l = i - 1;
+            int r = i;
+            while (++r < chars.length && chars[r] == chars[i]) ;
+            i = r;
+            while (l >= 0 && r < chars.length && chars[l] == chars[r]) {
+                l--;
+                r++;
+            }
+            int len = r - l - 1;
+            if (len > maxLength) {
+                maxLength = len;
+                begin = l + 1;
+            }
+        }
+        return new String(chars, begin, maxLength);
+    }
+
     public static void main(String[] args) {
         _5_最长回文子串 o = new _5_最长回文子串();
         System.out.println(o.longestPalindrome("babad"));
