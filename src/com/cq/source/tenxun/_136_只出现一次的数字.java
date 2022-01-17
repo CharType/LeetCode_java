7703079db50527e5879ffdfd0dbf211c1afd2dfc
chengqian <chartype@163.com>
2022-01-17 22:25:43 +0800

    判断只出现一次的数字

--- src/com/cq/source/tenxun/_136_只出现一次的数字.java
+++ src/com/cq/source/tenxun/_136_只出现一次的数字.java
@@ -3,10 +3,16 @@ package com.cq.source.tenxun;
 public class _136_只出现一次的数字 {
 
     public int singleNumber(int[] nums) {
-        return 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-
+        _136_只出现一次的数字 o = new _136_只出现一次的数字();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(o.singleNumber(nums));
     }
 }
